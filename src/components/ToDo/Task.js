40beee0e9a65f01f452d018a0879c13a44a50847
Arek <artek547@gmail.com>
2018-08-02 19:15:02 +0200

    material-ui

--- src/components/ToDo/Task.js
+++ src/components/ToDo/Task.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import MenuItem from 'material-ui/MenuItem';
+import Delete from 'material-ui/svg-icons/action/delete';
 
 const Task = (props) => (
-    <div>Task</div>
+    <MenuItem
+    primaryText={`Section task`}
+    rightIcon={<Delete onClick={() => alert('Click')}/>}
+    />
 )
 
 export default Task
