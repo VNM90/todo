644895c04a4bc6b9fd77d5177f8b25dc8fd8eac1
Arek <artek547@gmail.com>
2018-08-02 18:20:33 +0200

    structure

---
+++ src/components/ToDo/ToDo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Form from './Form'
+import List from './List'
+
+class ToDo extends React.Component {
+    render() {
+        return (
+            <div>
+                <Form />
+                <List />
+            </div>
+        )
+    }
+}
+
+export default ToDo
